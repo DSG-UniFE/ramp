b4bb4fc603de4f1711a9a5a114bf7cc16f8b47de
giulio <giulio.ribe@gmail.com>
2017-05-02 12:53:48 +0200

    Removed ImagesMismatchException from util package

--- src/it/unibo/deis/lia/ramp/util/ImagesMismatchException.java
+++
@@ -1,8 +0,0 @@
-package it.unibo.deis.lia.ramp.util;
-
-public class ImagesMismatchException extends Exception {
-	public ImagesMismatchException() { super(); }
-	public ImagesMismatchException(String message) { super(message); }
-	public ImagesMismatchException(String message, Throwable cause) { super(message, cause); }
-	public ImagesMismatchException(Throwable cause) { super(cause); }
-}
