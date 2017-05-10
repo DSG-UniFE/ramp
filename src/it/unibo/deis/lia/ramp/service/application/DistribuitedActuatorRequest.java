b392eab146f21f61544639c23d8cb7a020cdf9b5
giulio <giulio.ribe@gmail.com>
2017-05-10 15:30:07 +0200

    Improved DistribuitedActuatorRequest

--- src/it/unibo/deis/lia/ramp/service/application/DistribuitedActuatorRequest.java
+++ src/it/unibo/deis/lia/ramp/service/application/DistribuitedActuatorRequest.java
@@ -3,16 +3,19 @@ package it.unibo.deis.lia.ramp.service.application;
 import java.io.Serializable;
 
 public class DistribuitedActuatorRequest implements Serializable {
-	
 	/**
 	 * 
 	 */ 
 	private static final long serialVersionUID = 4859525002395755276L;
 	
 	private Type type;
+	private String appName;
+	private int port;
 	
-	protected DistribuitedActuatorRequest (Type type) {
-    	
+	protected DistribuitedActuatorRequest (String appName, Type type, int port) {
+    	this.appName = appName;
+    	this.type = type;
+    	this.port = port;
     }
 	
 	public Type getType() {
@@ -24,10 +27,25 @@ public class DistribuitedActuatorRequest implements Serializable {
 		this.type = type;
 	}
 
+	public String getAppName() {
+		return appName;
+	}
 
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
 	
 	public enum Type {
-		S_AVAILABLE_APPS, S_PRE_COMMAND, S_COMMAND,
-		C_HERE_I_AM, C_JOIN, C_LEAVE, C_WHICH_APP
+		AVAILABLE_APPS, PRE_COMMAND, COMMAND,
+		HERE_I_AM, JOIN, LEAVE, WHICH_APP
 	}
 }
