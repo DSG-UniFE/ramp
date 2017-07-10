73f6f426f9e3c651693d22a0c03aa012c4d25c1f
giulio <giulio.ribe@gmail.com>
2017-07-10 15:02:37 +0200

    Fixed class for benchmarks

--- src/it/unibo/deis/lia/ramp/util/Benchmark.java
+++ src/it/unibo/deis/lia/ramp/util/Benchmark.java
@@ -2,125 +2,141 @@ package it.unibo.deis.lia.ramp.util;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Arrays;
+import java.util.Date;
+
+import org.apache.commons.io.comparator.NameFileComparator;
 
 import com.opencsv.CSVWriter;
 
 import it.unibo.deis.lia.ramp.RampEntryPoint;
 
 public class Benchmark {
-	
-	private String filename = "benchmark";
-	private ArrayList<String> benchmarksList;
-	private String bench_dir = "temp/benchmark";
-	private static SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	private static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
-	private static SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss");
-	
-	public Benchmark(String filename) {
-		this.benchmarksList  = new ArrayList<String>();
-		this.setFilename(filename);
-		this.createDirectory();
-		this.createFile();
-		
-	}
-	
-	private void createDirectory() { 
-		if (RampEntryPoint.getAndroidContext() != null) {
-			bench_dir = android.os.Environment.getExternalStorageDirectory() + "/benchmark";
-			File dir = new File(bench_dir);
-			if(!dir.exists())
-				dir.mkdirs();
-		}
-	 }
 
-	private void createFile() {
-		Calendar date = Calendar.getInstance();
-		CSVWriter writer;
+	private static String DATE_TIME_FORMAT = new String("yyyy-MM-dd HH:mm:ss");
+	private static String DATE_FORMAT = new String("yyyy_MM_dd");
+	private static String TIME_FORMAT = new String("HH:mm:ss");
+
+	private static String HEAD = new String("Date#Time#Milliseconds#Type#Packet ID#Sender#Recipient");
+	private static String BENCH_DIR = "./logs/";
+	private static String FILENAME = null;
+	private static String FILE_EXTENSION = ".csv";
+	private static String BENCH_PATH = null;
+	private static String ENDS_WITH = "benchmark";
+
+	public static void createDirectory() {
 		try {
-//			writer = new CSVWriter(new FileWriter(DATE_TIME_FORMAT.format(date.getTime()) + "-" + filename + ".csv"), ';');
-			String filenameSaved = bench_dir + filename + ".csv";
-			writer = new CSVWriter(new FileWriter(filenameSaved, true), ';');
-			// feed in your array (or convert your data to an array)
-		    String[] entries = "Date#Time#Milliseconds#Type#Packet ID#Sender#Recipient".split("#");
-		    writer.writeNext(entries);    
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void writeFile() {
-		if (RampEntryPoint.getAndroidContext() != null) {
-			Calendar date = Calendar.getInstance();
-		       
-			CSVWriter writer;
-			try {
-				
-				writer = new CSVWriter(new FileWriter(DATE_TIME_FORMAT.format(date.getTime()) + filename + ".csv"), ';');
-				// feed in your array (or convert your data to an array)
-			    String[] entries = "Date#Time#Packet ID#Sender#Recipient".split("#");
-			    writer.writeNext(entries);
-			    
-			    for (String temp : getBenchmarks()) {
-			    	writer.writeNext(temp.split("#"));
+			if (RampEntryPoint.getAndroidContext() != null) {
+				System.out.println("--->SONO DENTRO");
+				if (android.os.Environment.getExternalStorageDirectory() != null) {
+					BENCH_DIR = android.os.Environment.getExternalStorageDirectory() + "/ramp/logs/";
+
+					File dir = new File(BENCH_DIR);
+					if (!dir.exists())
+						dir.mkdirs();
 				}
-			    
-				writer.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+			} else {
+				if (Files.notExists(Paths.get(BENCH_DIR), LinkOption.NOFOLLOW_LINKS)) {
+					Files.createDirectories(new File(BENCH_DIR).toPath());
+				}
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	public void appendBench(Calendar date, String packetId, String sender, String recipient) {
-		// long millis = System.currentTimeMillis();
-		String row = DATE_FORMAT.format(date.getTime()) + "#" + 
-				TIME_FORMAT.format(date.getTime()) + "#" +
-				packetId + "#" +
-				sender + "#" +
-				recipient;
-		
+
+	public static void createFile() {
+		createDirectory();
+
+		int nfiles = getNumberFiles(BENCH_DIR, ENDS_WITH, FILE_EXTENSION);
+		FILENAME = getDate(DATE_FORMAT.toString()) + "-" + String.format("%02d", nfiles) + "-" + ENDS_WITH
+				+ FILE_EXTENSION;
+		BENCH_PATH = BENCH_DIR + FILENAME;
+
+		System.out.println("FILENAME: " + FILENAME);
+		System.out.println("BENCH_DIR: " + BENCH_DIR);
+		System.out.println("BENCH_PATH: " + BENCH_PATH);
+
 		CSVWriter writer;
 		try {
-			String filenameSaved = bench_dir + filename + ".csv";
-			writer = new CSVWriter(new FileWriter(filenameSaved, true), ';');
-	    	writer.writeNext(row.split("#"));
+			// writer = new CSVWriter(new
+			// FileWriter(DATE_TIME_FORMAT.format(date.getTime()) + "-" +
+			// filename + ".csv"), ';');
+			writer = new CSVWriter(new FileWriter(BENCH_PATH, true), ';');
+			String[] entries = HEAD.split("#");
+			writer.writeNext(entries);
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public String getFilename() {
-		return filename;
+
+	public static void appendBenchmark(int millis, String type, String packetId, String sender, String recipient) {
+		File lastFile = getLastFilename(BENCH_DIR, ENDS_WITH, FILE_EXTENSION);
+
+		Date date = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		SimpleDateFormat timeFormat = new SimpleDateFormat(TIME_FORMAT);
+		String row = dateFormat.format(date.getTime()) + "#" + timeFormat.format(date.getTime()) + "#" + millis + "#"
+				+ type + "#" + packetId + "#" + sender + "#" + recipient;
+
+		CSVWriter writer;
+		try {
+			writer = new CSVWriter(new FileWriter(lastFile, true), ';');
+			writer.writeNext(row.split("#"));
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void setFilename(String filename) {
-		this.filename = filename;
+	public static File getLastFilename(String dirname, String endsWith, String fileExtension) {
+		File dir = new File(dirname);
+		File[] files = dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(endsWith + fileExtension);
+			}
+		});
+
+		Arrays.sort(files, NameFileComparator.NAME_INSENSITIVE_COMPARATOR);
+
+		if (files.length != 0)
+			return files[files.length - 1];
+
+		return null;
 	}
 
-	public void addBenchmark(Calendar date, long millis, String type, 
-			String packetId, String sender, String recipient) {
-		// long millis = System.currentTimeMillis();
-		String row = DATE_FORMAT.format(date.getTime()) + "#" + 
-				TIME_FORMAT.format(date.getTime()) + "#" +
-				millis + "#" +
-				type + "#" +
-				packetId + "#" +
-				sender + "#" +
-				recipient;
-		this.benchmarksList.add(row);
+	public static int getNumberFiles(String dirname, String endsWith, String fileExtension) {
+		File dir = new File(dirname);
+		File[] files = dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(endsWith + fileExtension);
+			}
+		});
+
+		Arrays.sort(files, NameFileComparator.NAME_INSENSITIVE_COMPARATOR);
+
+		return files.length;
 	}
-	
-	public void removeLastBenchmark() {
-		this.benchmarksList.remove(benchmarksList.size() - 1);
+
+	public static String getDate(String format) {
+		// example "HH:mm:ss", "yyyy-MM-dd'T'HH:mm:ss" ecc...
+		SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+		return dateFormat.format(new Date());
 	}
-	
-	public ArrayList<String> getBenchmarks() {
-		return benchmarksList;
+
+	private static String getFileExtension(String filename) {
+		if (filename.lastIndexOf(".") != -1 && filename.lastIndexOf(".") != 0)
+			return filename.substring(filename.lastIndexOf(".") + 1);
+		else
+			return "";
 	}
 }
